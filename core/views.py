5405075f0f4c60f6ab0e314d5fee96b52cf1909d
Lazaro Santos <lazarosantos@institutoeducar.com.br>
2021-03-30 17:10:39 -0300

    Primeiro commit

---
+++ core/views.py
@@ -0,0 +1,21 @@
+from .models import List, Item
+from rest_framework import viewsets
+from rest_framework import permissions, authentication
+from .serializers import ListSerializer, ItemSerializer
+
+
+class ListViewSet(viewsets.ModelViewSet):
+       serializer_class = ListSerializer
+       permission_classes = [permissions.IsAuthenticated]
+       authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
+       
+       def get_queryset(self):
+              user = self.request.user
+              return List.objects.filter(owner=user)
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+       queryset = Item.objects.all()
+       serializer_class = ItemSerializer
+       permission_classes = [permissions.IsAuthenticated]
+       authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
